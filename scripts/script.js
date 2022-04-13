ae08dcbf45d1e8fb523cff86275a30c70fb00797
stan99-cheb <stan99-cheb@yandex.ru>
2022-04-13 08:48:53 +0300

    13.04

--- scripts/script.js
+++ scripts/script.js
@@ -1,133 +1,59 @@
 
 
 block_popup: {
-  function popupCloseHandler(e) {
-    const closeButton = e.target.closest('.popup');
+  let activePopup, newCard;
 
-    closeButton.removeEventListener('click', popupCloseHandler);
-
-    popupOff(closeButton);
-  }
-
-  function popupOn(item) {
-    const closeButton = item.querySelector('.popup__close-button');
-
-    item.classList.toggle('popup_active');
-
-    closeButton.addEventListener('click', popupCloseHandler);
-  }
-
-  function popupOff(item) {
-    
-    item.classList.toggle('popup_active');
-  }
-}
-
-/*
-block_popup: {
-  let popup;
-  //Обработчик событий попапа
-  function submitButtonHandler() {
-    const header = document.querySelector('.form__header');
-    switch (header.textContent) {
-      case 'Редактировать профиль':
-        //Заполняем профиль новыми данными
-        document.querySelector('.profile__title').textContent = document.querySelector('.form__name-input').value;
-        document.querySelector('.profile__subtitle').textContent = document.querySelector('.form__job-input').value;
-        popup_out();
-        break;
-      case 'Новое место':
-        //Готовим объект
-        const newCard = {
-          name: document.querySelector('.form__name-input').value,
-          link: document.querySelector('.form__job-input').value
-        };
-        //Добавляем новую карту
-        addCard(newCard);
-        popup_out();
-        break;
-    }
-  }
-
-  function popupHandler(e) {
-    //Запрещаем действия по умолчанию
-    e.preventDefault();
-    //Получаем селектор цели
+  function activePopupHandler(e) {
+    const activePopup = e.target.closest('.popup');
+    const header = activePopup.querySelector('.form__header');
     const selectButton = e.target.closest('.popup__close-button') || e.target.closest('.form__submit-button');
-
+    
+    e.preventDefault();
+    
     if (selectButton) {
-      switch (selectButton.className) {
-        case 'popup__close-button':
-          popup_out();
-          break;
-        case 'form__submit-button':
-          //Пока нет обработчика этой кнопки
-          submitButtonHandler();
-          break;
+      if (selectButton.classList.contains('form__submit-button')) {
+        switch (header.textContent) {
+          case 'Редактировать профиль':
+            document.querySelector('.profile__title').textContent = e.currentTarget.querySelector('.form__name-input').value;
+            document.querySelector('.profile__subtitle').textContent = e.currentTarget.querySelector('.form__job-input').value;
+            activePopup.removeEventListener('click', activePopupHandler);
+            popupOff(activePopup);
+            break;
+          case 'Новое место':
+            const newCard =
+              {
+                name: e.currentTarget.querySelector('.form__name-input').value,
+                link: e.currentTarget.querySelector('.form__job-input').value
+              };
+            addCard(newCard);
+            activePopup.removeEventListener('click', activePopupHandler);
+            popupOff(activePopup);
+            break;
+        }
       }
     }
   }
 
-  function popup_on(form) {
-    //Получаем шаблон
-    const popupTemplate = document.querySelector('#popup-template');
-    //Клонируем попап из шаблона
-    let popupCopy = popupTemplate.content.cloneNode(true);
-    //добавляем в попап форму
-    popupCopy.querySelector('.popup__container').append(form);
-    //Подмешиваем селектор плавного появления окна
-    popupCopy.querySelector('.popup').classList.add('popup_fade-in');
-    //показываем форму на экране
-    document.body.append(popupCopy);
-    //Добавляем слушатель событий
-    popup = document.querySelector('.popup');
-    popup.addEventListener('click', popupHandler);
+  function popupOn(activePopup) {
+    activePopup.classList.toggle('popup_active');
+
+    activePopup.addEventListener('click', activePopupHandler);
   }
 
-  function popup_out() {
-    //Удаляем селектор плавного открытия
-    popup.classList.remove('popup_fade-in');
-    //Подмешиваем селектор плавного исчезновения
-    popup.classList.add('popup_fade-out');
-    //Убираем обработчик
-    popup.removeEventListener('click', popupHandler);
-    //Ждем окончания анимации
-    popup.addEventListener('animationend', () => {
-      //Удаляем попап
-      popup.remove();
-    }, {once: true});//Прослушиватель выполняется только один раз
+  function popupOff(activePopup) {
+    
+    activePopup.classList.toggle('popup_active');
   }
 }
-
-block_form: {
-  function fillForm(form, select) {
-    switch (select) {
-      case 'edit':
-        form.querySelector('.form__header').textContent = 'Редактировать профиль';
-        form.querySelector('.form__name-input').placeholder = 'Иван Иванов';
-        form.querySelector('.form__job-input').placeholder = 'О себе';
-        form.querySelector('.form__submit-button').textContent = 'Сохранить';
-        break;
-      case 'add':
-        form.querySelector('.form__header').textContent = 'Новое место';
-        form.querySelector('.form__name-input').placeholder = 'Название';
-        form.querySelector('.form__job-input').placeholder = 'Ссылка на картинку';
-        form.querySelector('.form__submit-button').textContent = 'Создать';
-        break;
-    }
-    return form;
-  }
-}
-*/
-
 
 block_profile: {
   const popupProfile = document.querySelector('.popup-profile');
-  const profile = document.querySelector('.profile__edit-button');
+  const editButton = document.querySelector('.profile__edit-button');
+
+  const popupCard = document.querySelector('.popup-card');
+  const addButton = document.querySelector('.profile__add-button');
 
   function clickEditProfileHandler(e) {
-    const editButton = e.target.closest('.profile__edit-button');
-    
     if (editButton) {
       popupProfile.querySelector('.form__name-input').value = document.querySelector('.profile__title').textContent;
       popupProfile.querySelector('.form__job-input').value = document.querySelector('.profile__subtitle').textContent;
@@ -135,7 +61,13 @@ block_profile: {
     }
   }
   //Вешаем обработчик на кнопку редактирования профиля
-  profile.addEventListener('click', clickEditProfileHandler);
+  editButton.addEventListener('click', clickEditProfileHandler);
+
+  function clickAddCardHandler() {
+    popupOn(popupCard);
+  }
+
+  addButton.addEventListener('click', clickAddCardHandler);
 }
 
 block_cards: {
@@ -186,6 +118,7 @@ block_cards: {
   });
 
   function cardsHundler(e) {
+    const activePopup = document.querySelector('.popup-image');
     //Выбраем нужный элемент
     const selectButton = e.target.closest('.card__link') || e.target.closest('.card__heart') || e.target.closest('.card__trash');
     //Если нажат нужный элемент
@@ -199,7 +132,8 @@ block_cards: {
           //Добавляем нужный
           cloneSelectButton.classList.add('popup__link');
           //Передаем элемент в попап и показываем попап
-          popup_on(cloneSelectButton);
+          activePopup.querySelector('.popup__container').append(cloneSelectButton);
+          popupOn(activePopup);
           break;
         case 'card__heart':
           //Переключаем сердечко
