b9c2465b2f018a1cb2ae9f8add5772398dcafaec
stan99 <stan99-cheb@yandex.ru>
2022-08-08 21:30:38 +0300

    refactor/UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,28 +1,17 @@
 export default class UserInfo {
     constructor(user) {
-        this._userName = user.name;
-        this._userAbout = user.about;
-        this._userAvatar = user.avatar;
-        this._userId = "";
+        this._user = user;
     }
 
     getUserInfo() {
-        return {
-            name: this._userName,
-            about: this._userAbout,
-            avatar: this._userAvatar,
-            _id: this._userId
-        }
+        return this._user;
     }
 
     setUserInfo(user) {
-        this._userName = user.name;
-        this._userAbout = user.about;
-        this._userAvatar = user.avatar;
-        this._userId = user._id;
+        this._user = user;
 
-        document.querySelector('.profile__title').textContent = this._userName;
-        document.querySelector('.profile__subtitle').textContent = this._userAbout;
-        document.querySelector('.profile__avatar').style.backgroundImage = `url(${this._userAvatar})`;
+        document.querySelector('.profile__title').textContent = this._user.name;
+        document.querySelector('.profile__subtitle').textContent = this._user.about;
+        document.querySelector('.profile__avatar').style.backgroundImage = `url(${this._user.avatar})`;
     }
 }
